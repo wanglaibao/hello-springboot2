1a593805b720b30f77313c15ea726a4ea2d2b868
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-12-21 21:13:03 +0800

    update pom.xml file info

---
+++ simple-mybatis-app/src/main/java/com/laibao/springboot2/simplemybatisapp/SimpleMybatisAppApplication.java
@@ -0,0 +1,11 @@
+package com.laibao.springboot2.simplemybatisapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMybatisAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMybatisAppApplication.class, args);
+	}
+}
---
+++ simple-mybatis-app/src/test/java/com/laibao/springboot2/simplemybatisapp/SimpleMybatisAppApplicationTests.java
@@ -0,0 +1,34 @@
+package com.laibao.springboot2.simplemybatisapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleMybatisAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("应用启动成功了");
+	}
+
+	@Test
+	public void testMySqlForQuery() {
+
+	}
+
+
+	@Test
+	public void testMySqlForUpdate() {
+
+	}
+
+	@Test
+	public void testGetAllSysUsers() {
+
+	}
+
+}
+
