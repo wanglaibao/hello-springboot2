68595457943ae5e188473b17a7ccab8f2b22c58d
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-12-21 12:36:27 +0800

    update pom.xml file info

---
+++ simple-mysql-app/src/main/java/com/laibao/springboot2/simplemysqlapp/SimpleMySqlAppApplication.java
@@ -0,0 +1,11 @@
+package com.laibao.springboot2.simplemysqlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMySqlAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMySqlAppApplication.class, args);
+	}
+}
