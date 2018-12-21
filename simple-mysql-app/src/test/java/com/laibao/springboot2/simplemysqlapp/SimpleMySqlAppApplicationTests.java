9582fce64016eb2d3b8b232cf4d963945d27f617
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-12-21 12:49:36 +0800

    update pom.xml file info

--- simple-mysql-app/src/test/java/com/laibao/springboot2/simplemysqlapp/SimpleMySqlAppApplicationTests.java
+++ simple-mysql-app/src/test/java/com/laibao/springboot2/simplemysqlapp/SimpleMySqlAppApplicationTests.java
@@ -1,9 +1,15 @@
 package com.laibao.springboot2.simplemysqlapp;
 
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,5 +20,16 @@ public class SimpleMySqlAppApplicationTests {
 		System.out.println("应用启动成功了");
 	}
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Test
+	public void testMySqlQuery() {
+		List<Map<String,Object>> result = jdbcTemplate.queryForList("select * from sys_user");
+		System.out.println("query result is : " + result.size());
+		System.out.println("success");
+		System.out.println();
+		System.out.println(JSON.toJSONString(result));
+	}
 }
 
