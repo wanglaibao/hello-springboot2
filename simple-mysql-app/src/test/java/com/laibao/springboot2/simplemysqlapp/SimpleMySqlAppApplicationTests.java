8dcf4092fe821226e0a5406d6f5d090ba2f0fd36
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-12-21 13:05:32 +0800

    update pom.xml file info

--- simple-mysql-app/src/test/java/com/laibao/springboot2/simplemysqlapp/SimpleMySqlAppApplicationTests.java
+++ simple-mysql-app/src/test/java/com/laibao/springboot2/simplemysqlapp/SimpleMySqlAppApplicationTests.java
@@ -24,12 +24,24 @@ public class SimpleMySqlAppApplicationTests {
 	private JdbcTemplate jdbcTemplate;
 
 	@Test
-	public void testMySqlQuery() {
+	public void testMySqlForQuery() {
 		List<Map<String,Object>> result = jdbcTemplate.queryForList("select * from sys_user");
 		System.out.println("query result is : " + result.size());
 		System.out.println("success");
 		System.out.println();
 		System.out.println(JSON.toJSONString(result));
+	}
+
+
+	@Test
+	public void testMySqlForUpdate() {
+		try {
+			jdbcTemplate.execute("update sys_user set investor_id = 'aaaaa',user_name = 'user00100',phone = '123123123',email = '123123123@qq.com' where user_id = 'user001'");
+			System.out.println("success");
+		}catch (RuntimeException ex) {
+			System.out.println("failure");
+			throw ex;
+		}
 	}
 }
 
