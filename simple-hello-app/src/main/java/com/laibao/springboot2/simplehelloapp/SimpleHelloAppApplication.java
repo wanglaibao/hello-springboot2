2d055d31741b2d0a30a6c38a37c02fa8e54201f1
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-12-21 11:03:59 +0800

    update pom.xml file info

---
+++ simple-hello-app/src/main/java/com/laibao/springboot2/simplehelloapp/SimpleHelloAppApplication.java
@@ -0,0 +1,12 @@
+package com.laibao.springboot2.simplehelloapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHelloAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleHelloAppApplication.class, args);
+	}
+}
+
