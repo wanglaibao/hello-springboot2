14cfd82efc85d435a6620905cf682a9cd81200a4
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-12-22 12:38:21 +0800

    add new java files info

---
+++ traditional-mybatis-app/src/main/java/com/laibao/springboot2/traditionalmybatisapp/TraditionalMybatisAppApplication.java
@@ -0,0 +1,11 @@
+package com.laibao.springboot2.traditionalmybatisapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraditionalMybatisAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TraditionalMybatisAppApplication.class, args);
+	}
+}
