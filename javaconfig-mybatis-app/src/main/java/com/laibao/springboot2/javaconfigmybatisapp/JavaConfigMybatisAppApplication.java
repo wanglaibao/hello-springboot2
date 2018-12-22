9cf2de88dc3b2b9564d57d41216cd51e497b6bf8
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-12-22 10:24:57 +0800

    add new java files info

---
+++ javaconfig-mybatis-app/src/main/java/com/laibao/springboot2/javaconfigmybatisapp/JavaConfigMybatisAppApplication.java
@@ -0,0 +1,11 @@
+package com.laibao.springboot2.javaconfigmybatisapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConfigMybatisAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConfigMybatisAppApplication.class, args);
+	}
+}
