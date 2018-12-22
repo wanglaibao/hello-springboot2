3097fdbb704b7d9c09070d0a2d551cb2ba05ae7a
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-12-22 08:30:33 +0800

    update pom.xml file info

--- simple-mybatis-app/src/main/java/com/laibao/springboot2/simplemybatisapp/service/SysDistrictService.java
+++ simple-mybatis-app/src/main/java/com/laibao/springboot2/simplemybatisapp/service/SysDistrictService.java
@@ -1,13 +1,11 @@
 package com.laibao.springboot2.simplemybatisapp.service;
 
 import com.laibao.springboot2.simplemybatisapp.domain.SysDistrict;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
 
 public interface SysDistrictService {
 
-    List<SysDistrict> selectByCityId(@Param("cityId") String cityId);
+    List<SysDistrict> selectByCityId(String cityId);
 
     SysDistrict selectByPrimaryKey(SysDistrict key);
 }
--- simple-mybatis-app/src/test/java/com/laibao/springboot2/simplemybatisapp/SimpleMybatisAppApplicationTests.java
+++ simple-mybatis-app/src/test/java/com/laibao/springboot2/simplemybatisapp/SimpleMybatisAppApplicationTests.java
@@ -1,13 +1,32 @@
 package com.laibao.springboot2.simplemybatisapp;
 
+import com.alibaba.fastjson.JSON;
+import com.laibao.springboot2.simplemybatisapp.domain.SysCity;
+import com.laibao.springboot2.simplemybatisapp.domain.SysDistrict;
+import com.laibao.springboot2.simplemybatisapp.domain.SysProvince;
+import com.laibao.springboot2.simplemybatisapp.service.SysCityService;
+import com.laibao.springboot2.simplemybatisapp.service.SysDistrictService;
+import com.laibao.springboot2.simplemybatisapp.service.SysProvinceService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SimpleMybatisAppApplication.class)
 public class SimpleMybatisAppApplicationTests {
+
+	@Autowired
+	private SysCityService sysCityService;
+
+	@Autowired
+	private SysDistrictService sysDistrictService;
+
+	@Autowired
+	private SysProvinceService sysProvinceService;
 
 	@Test
 	public void contextLoads() {
@@ -15,20 +34,34 @@ public class SimpleMybatisAppApplicationTests {
 	}
 
 	@Test
-	public void testMySqlForQuery() {
-
-	}
-
-
-	@Test
-	public void testMySqlForUpdate() {
-
+	public void testGetProvinceById() {
+		String provinceId= "340000";
+		SysProvince sysProvince = sysProvinceService.selectByPrimaryKey(provinceId);
+		System.out.println(JSON.toJSONString(sysProvince));
 	}
 
 	@Test
-	public void testGetAllSysUsers() {
-
+	public void testGetSysCityListByProvinceId() {
+		String provinceId= "340000";
+		List<SysCity> sysCityList =  sysCityService.selectByProvinceId(provinceId);
+		sysCityList.forEach(sysCity -> System.out.println(JSON.toJSONString(sysCity)));
 	}
 
+	@Test
+	public void testGetSysDistrictByPrimaryKey() {
+		SysDistrict key = new SysDistrict();
+		key.setDistrictId("340811");
+		key.setCityId("340800");
+
+		SysDistrict sysDistrict = sysDistrictService.selectByPrimaryKey(key);
+		System.out.println(JSON.toJSONString(sysDistrict));
+	}
+
+	@Test
+	public void testGetSysDistrictListByCityId() {
+		String cityId = "340800";
+		List<SysDistrict> sysDistrictList = sysDistrictService.selectByCityId(cityId);
+		sysDistrictList.forEach(sysDistrict -> System.out.println(JSON.toJSONString(sysDistrict)));
+	}
 }
 
